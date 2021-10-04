85abca58cc96edbb57d1fdcb8c92507cc8f41a5f
surajadkhari <surajadkhari@gmail.com>
2021-10-04 17:11:33 +0545

    use_riverpod_futureprovider fetch data

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'screens/news_detial_screen.dart';
 import 'screens/news_screen.dart';
+import 'screens/newshome_riverpod.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(ProviderScope(child: Myapp()));
 }
 
 class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Newscreen(),
+      home: NewsHomepage(),
       debugShowCheckedModeBanner: false,
       routes: {
         "/detailpafe": (context) => newsDetail(),
---
+++ lib/services/services_riverpod.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart';
+import 'package:newspaper_app/model/news_mode.dart';
+
+class Db {
+  Future<NewsModel> fetchNews() async {
+    var endPoint =
+        "https://newsapi.org/v2/everything?q=Apple&from=2021-09-12&sortBy=popularity&apiKey=827dea572df845ea9be0bd26e85f0746";
+    var response = await get(Uri.parse(endPoint));
+    if (response.statusCode == 200) {
+      NewsModel newsData = newsModelFromJson(response.body);
+      return newsData;
+    } else {
+      return null;
+    }
+  }
+}
+
+final networkdataserviceProvider = Provider((ref) => Db());
